97e39e7f6e1f9898159fb2ff67c80401dc6e98aa
junior272006 <junior27nguetta@gmail.com>
2026-01-15 20:16:14 +0000

    logo

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,8 @@ mongoose.connect(process.env.MONGO_URI)
 // CORS
 app.use(cors({
   origin: [
-    '*'
+      'http://localhost:5173',
+    'https://speakmate-seven.vercel.app'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
